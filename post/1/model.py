02b0a3aeca88de2a29f8b092e86c2c9f4d07793a
Phelan164 <phelan.nguyen@instill.tech>
2023-02-06 13:58:10 +0700

    chore: update import

--- post/1/model.py
+++ post/1/model.py
@@ -8,9 +8,8 @@ from typing import List
 
 import labels as CSLabels
 
-from triton_python_backend_utils import Tensor, InferenceResponse, \
-    get_input_tensor_by_name, InferenceRequest, get_input_config_by_name, \
-    get_output_config_by_name, triton_string_to_numpy
+from triton_python_backend_utils import get_output_config_by_name, triton_string_to_numpy, get_input_config_by_name
+from c_python_backend_utils import Tensor, InferenceResponse, InferenceRequest
 
 
 def rle_encode(im_arr):
